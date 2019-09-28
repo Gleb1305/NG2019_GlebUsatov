523891027497a0537e1abd818cd86a48ffdcea1f
Gleb1305 <gleb8062@gmai.com>
2019-09-28 11:50:17 +0300

    Addet first task

---
+++ Leson_1/1.1.1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int a;
+    int b;
+cout << "Vvedite chislo 1:";
+    cin >> a;
+cout << "Vvedite chislo 2:";
+    cin >> b;
+cout << "Result: ";
+    cout <<a+b;
+}
